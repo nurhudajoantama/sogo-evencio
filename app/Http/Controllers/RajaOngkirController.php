96361f6b53afb945ab9b4dfe5c370cee78047126
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-11-03 20:22:31 +0700

    rajaongkir integration

---
+++ app/Http/Controllers/RajaOngkirController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RajaOngkirController extends Controller
+{
+    public function getProvince(Request $request)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', 'https://api.rajaongkir.com/starter/province?id=' . $request->id, [
+            'headers' => [
+                'key' => config('apikey.raja_ongkir_api_key')
+            ],
+        ]);
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result['rajaongkir']['results'];
+    }
+
+    public function getCity(Request $request)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', 'https://api.rajaongkir.com/starter/city?id=' . $request->id . '&province=' . $request->province, [
+            'headers' => [
+                'key' => config('apikey.raja_ongkir_api_key')
+            ],
+        ]);
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result['rajaongkir']['results'];
+    }
+
+    public function getCost(Request $request)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('POST', 'https://api.rajaongkir.com/starter/cost', [
+            'headers' => [
+                'key' => config('apikey.raja_ongkir_api_key'),
+                'content-type' => 'application/x-www-form-urlencoded'
+            ],
+            'form_params' => [
+                'origin' => $request->origin,
+                'destination' => $request->destination,
+                'weight' => $request->weight,
+                'courier' => $request->courier
+            ]
+        ]);
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result['rajaongkir']['results'];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RajaOngkirController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/rajaongkir/provinces', [RajaOngkirController::class, 'getProvince'])->name('rajaongkir.provinces');
+Route::get('/rajaongkir/cities', [RajaOngkirController::class, 'getCity'])->name('rajaongkir.cities');
+Route::post('/rajaongkir/cost', [RajaOngkirController::class, 'getCost'])->name('rajaongkir.cost');
